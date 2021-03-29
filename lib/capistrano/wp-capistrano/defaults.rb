c5e236e66f0f899d00fb4dbedd7f096b6a25eb76
Nicolas Renault <nrenault@tangkoko.com>
2021-03-29 21:27:55 +0200

    fix bugs

--- lib/capistrano/wp-capistrano/defaults.rb
+++ lib/capistrano/wp-capistrano/defaults.rb
@@ -2,5 +2,5 @@
 before 'deploy:check:linked_files', 'wp-capistrano:create_wp_config'
 before 'deploy:updating', 'wp-capistrano:download_wordpress'
 after 'wp-capistrano:download_wordpress', 'wp-capistrano:install_wordpress'
-#after 'wp-capistrano:download_wordpress', 'wp-capistrano:update_wordpress'
+after 'wp-capistrano:download_wordpress', 'wp-capistrano:update_wordpress'
 #before 'deploy:updated', 'wp-capistrano:deploy-theme'
