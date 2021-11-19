9fabb97a5dd743da88a20b9e5048860cfea7689c
Nicolas Renault <nrenault@tangkoko.com>
2021-11-19 11:38:20 +0100

    0.0.16

--- lib/capistrano/wp-capistrano/defaults.rb
+++ lib/capistrano/wp-capistrano/defaults.rb
@@ -2,5 +2,5 @@
 before 'deploy:check:linked_files', 'wp-capistrano:create_wp_config'
 before 'deploy:updating', 'wp-capistrano:download_wordpress'
 after 'wp-capistrano:download_wordpress', 'wp-capistrano:install_wordpress'
-after 'wp-capistrano:install_wordpress', 'wp-capistrano:update_wordpress'
+after 'wp-capistrano:install_plugins', 'wp-capistrano:update_wordpress'
 after 'deploy:updating', 'wp-capistrano:install_plugins'
--- wp-capistrano.gemspec
+++ wp-capistrano.gemspec
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
   spec.name          = 'wp-capistrano3'
-  spec.version       = '0.0.15'
+  spec.version       = '0.0.16'
   spec.authors       = ['Nicolas RENAULT']
   spec.email         = ['nrenault@tangkoko.com']
   spec.description   = %q{Wordpress tasks for Capistrano 3.x}
